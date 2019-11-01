4a065739ab91986e1632bc6d8ff994ce3b0986cd
Shingo Omura <everpeace@gmail.com>
2019-11-01 19:56:42 +0900

    fixup! provides more environs about leader election results to tasks

--- test_task.sh
+++ test_task.sh
@@ -4,7 +4,7 @@ echo
 echo "I AM ${__LEADER_ELECTOR_ROLE}"
 echo "MY IDENTITY IS ${__LEADER_ELECTOR_MY_IDENTITY}"
 echo "LEADER IS ${__LEADER_ELECTOR_LEADER}"
-echo "ELECTED AT ${__LEADER_ELECTOR_ACQUIRE_TIME}"
+echo "ELECTED AT ${__LEADER_ELECTOR_ACQUIRE_UNIXTIME}"
 echo
 echo "==========================================="
 
